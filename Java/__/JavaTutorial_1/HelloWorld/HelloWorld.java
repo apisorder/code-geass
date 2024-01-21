414c76a22b20439ada25d58434d5e1f507ccd293
Jeff Cheng <apisorder@gmail.com>
2024-01-20 21:40:16 -0800

    JavaTutorial_1

---
+++ Java/__/JavaTutorial_1/HelloWorld/HelloWorld.java
@@ -0,0 +1,67 @@
+
+public class HelloWorld
+{
+    //  class variable, accessible to all other varaiables and methods within the class
+    static String randomString = "a random string";
+
+    //  varaible that will always stay the same; or constant
+    static final double PINUM = 3.141529;
+
+    // static -> only a class can call it
+    public static void main(String[] args)
+    //  any main function must accept an array of String objects
+    {
+        //  declaration statement
+        int integerOne = 22;
+        //  expression statement
+        int integerTwo 
+        = 
+        integerOne 
+        + 1;
+
+        //  java doesn't care about whitespace
+
+        //  primitive types -> differences = what values they can hold
+        byte bigByte = 127;
+        short bigShort = 32767;
+        int bigInt = 2100000000;
+        long bigLong = 9220000000000000000L;
+        float bigFloat = 3.14F;
+
+        //  double is a bigger, more precise float
+        double bigDouble = 3.14823794872734872347923748;
+
+        System.out.println(randomString);
+        System.out.println(PINUM);
+
+        //  these are system dependent
+        //  precise to about 6 significant places
+        System.out.println(Float.MAX_VALUE);
+        //  precise to about 15 decimal places
+        System.out.println(Double.MAX_VALUE);
+
+        boolean trueOrFalse = true;
+
+        char randomChar = 65;
+        char anotherChar = 'A';
+
+        String randomString2 =  "I'm another random string!";
+        String anotherString = "Stuff";
+        
+        String andAnotherString = randomString2 + ' ' + anotherString;
+
+        String byteString = Byte.toString(bigByte);
+        String shortString = Short.toString(bigByte);
+        String intString = Integer.toString(bigByte);
+        String longString = Long.toString(bigByte);
+        String floatString = Float.toString(bigByte);
+        String doubleString = Double.toString(bigByte);
+
+        int stringToInt = Integer.parseInt(intString);
+
+        System.out.println(randomChar);
+        System.out.println(anotherChar);
+        System.out.println(byteString);
+        System.out.println(stringToInt);
+    }
+}
