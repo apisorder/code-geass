150e3f4761dbf811a3be3767a7f657adcbd8e9c6
Jeff Cheng <apisorder@gmail.com>
2024-01-22 15:33:09 -0800

    number_even_or_odd and swap_two_numbers Python REVIEWED

--- Python/__fundamentals/bit-manipulation/number-even-or-odd/number-even-or-odd.py
+++ Python/__fundamentals/bit-manipulation/number-even-or-odd/number-even-or-odd.py
@@ -1,23 +1,30 @@
 
+'''
+ *  Programmer:                     Jeff C. Cheng
+ *  Last modified:                  3:29PM 1-22-2024
+ *  Problem:                        Determine Number Is Even Or Odd (Zero)
+'''
+
 class Solution:
     def number_even_or_odd_slow(self, number):
         if number % 2 == 0:
-            return "Even"
+            return "even"
         else:
-            return "Odd"
+            return "odd"
     
     def number_even_or_odd_fast(self, number):
         if number & 1 == 0:
-            return "Even"
+            return "even"
         else:
-            return "Odd"
+            return "odd"
 
 solution = Solution
-print(solution.number_even_or_odd_slow(solution, 3));
-print(solution.number_even_or_odd_slow(solution, 14));
 
-print(solution.number_even_or_odd_fast(solution, 3));
-print(solution.number_even_or_odd_fast(solution, 14));
+number1 = 3
+number2 = 14
+
+print( f'First number has the value of {number1} and is {solution.number_even_or_odd_slow(solution, number1)}')
+print( f'Second number has the value of {number2} and is {solution.number_even_or_odd_fast(solution, number2)}')
 
 #   2 -> 10
 #   4 -> 100
